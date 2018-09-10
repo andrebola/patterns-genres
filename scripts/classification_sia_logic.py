9fdde1e671e2b1522fb79175ed51f12d9e5778b9
andrebola <andres.ferraro@upf.edu>
2018-09-10 16:48:11 +0200

    Add final patterns

--- scripts/classification_sia_logic.py
+++ scripts/classification_sia_logic.py
@@ -5,6 +5,7 @@ from collections import Counter
 from sklearn.svm import SVC, LinearSVR
 from sklearn.linear_model import SGDClassifier, LogisticRegression
 import numpy as np
+from sklearn.feature_extraction import DictVectorizer
 from sklearn import metrics
 from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder 
 from sklearn.model_selection import StratifiedKFold, cross_validate
@@ -79,16 +80,16 @@ def load_data (tonic_path, only_rhythm_path, genres_map, tpqn_map):
                 lakh_id = filename.replace(".json", "")
 
                 if lakh_id in genres_map[genre_dataset]:
-                    for genre in genres_map[genre_dataset][lakh_id]:
                         for key in tonic_pattern.keys():
                             if key not in ["jsDsRhythm", "dsChroma"]:
-                                normalized_patterns = set()
+                                normalized_patterns = {}
                                 for patterns_list in tonic_pattern[key]:
                                     if len(patterns_list) >2 and len(patterns_list) < 20:
 
                                         encoded_pattern = encode_pattern_both(patterns_list, tpqn_map[lakh_id])
                                         if encoded_pattern != "":
-                                            normalized_patterns.add(encoded_pattern)   
+                                            normalized_patterns.setdefault(encoded_pattern, 0)
+                                            normalized_patterns[encoded_pattern] += 1
                                 key_rhythm = "PatternsRythm2"
                                 if key_rhythm in pattern_rhythm:
                                     for patterns_list in pattern_rhythm[key_rhythm]:
@@ -96,12 +97,13 @@ def load_data (tonic_path, only_rhythm_path, genres_map, tpqn_map):
 
                                             encoded_pattern = encode_pattern_only_rhythm(patterns_list, tpqn_map[lakh_id])
                                             if encoded_pattern != "":
-                                                normalized_patterns.add(encoded_pattern)
+                                                normalized_patterns.setdefault(encoded_pattern, 0)
+                                                normalized_patterns[encoded_pattern] += 1
                                 if len(normalized_patterns):
                                     genres_patterns.setdefault(key, [])
                                     genres_patterns_labels.setdefault(key, [])
-                                    genres_patterns_labels[key].append(genre)
-                                    genres_patterns[key].append(list(normalized_patterns))
+                                    genres_patterns[key].append(normalized_patterns)
+                                    genres_patterns_labels[key].append(set(genres_map[genre_dataset][lakh_id]))
         except ValueError:
             pass
     return genres_patterns, genres_patterns_labels
@@ -109,26 +111,26 @@ def load_data (tonic_path, only_rhythm_path, genres_map, tpqn_map):
 def classify(genres_patterns, genres_patterns_labels):
     results = {}
     for key in genres_patterns.keys():
-        mlb = MultiLabelBinarizer(sparse_output=True)
-        conv_X_train = mlb.fit_transform(genres_patterns[key])
-
-        np.delete(conv_X_train, np.where(conv_X_train.sum(1)<5), 1)
+        tr = DictVectorizer()
+        conv_X_train = tr.fit_transform(genres_patterns[key])
         clb = MultiLabelBinarizer()
         y = clb.fit_transform(genres_patterns_labels[key])
+        skf = StratifiedKFold(n_splits=3)
 
-        clf = OneVsRestClassifier(LogisticRegression(class_weight='balanced'))
+        clf = OneVsRestClassifier(LogisticRegression(C=1,class_weight='balanced'))
         scoring = {'precision': 'precision_weighted',
                    'recall': 'recall_weighted',
                    'accuracy': 'accuracy',
                    'prec_sampl': 'precision_samples',
                    'rec_sampl': 'recall_samples',
                    'f1_samples': 'f1_samples',
-                   'f1_weighted': 'f1_weighted'
+                   'f1_weighted': 'f1_weighted',
+                   'roc_auc': 'roc_auc'
          }
 
         scores = cross_validate(clf, conv_X_train, y, cv=5, scoring=scoring, return_train_score=True)
         for k in scores.keys():
-            scores[k] = scores[k].tolist() 
+            scores[k] = scores[k].tolist()
         results[key] = scores
         json.dump(results, open("/tmp/classification_tonic.json", "w"))
 
